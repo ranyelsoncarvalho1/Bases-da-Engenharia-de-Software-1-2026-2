4fb04c6c2b3e679e31f33d978981df8a578c9c72
ranyelsoncarvalho1 <ranyelson.carvalho@undf.edu.br>
2026-09-21 11:43:09 -0300

    Create exemplo1.cpp

---
+++ aula-21-09-2026/exemplo1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	//exemplo de impressão
+	printf("Olá mundo!\n");
+	
+	return 0;
+	/*
+		múltiplos comentários
+		retornar 0, para indicar que a função está correta
+		o SO consegue agora executar.
+	*/
+}
